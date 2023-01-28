6cf6d3ba3b84257c08f7f18c828d500e7dbf9204
janWICKEDpro <janroyal45@gmail.com>
2023-01-28 04:35:00 +0100

    convert file Signature

--- lib/business_logic/cloud_convert/cloudconver_api_methods.dart
+++ lib/business_logic/cloud_convert/cloudconver_api_methods.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+
+import "package:http/http.dart" as http;
+
+import '../../bloc/file_selection_bloc/file_state.dart';
+
 class CloudConvertMethods {
   //import file
+  Future<Map<String, dynamic>> convert(FileState file) async {
+    return {};
+  }
 }
