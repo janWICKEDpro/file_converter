3fcc2b82b34103addb7226bec4985e0c353d2ab0
janWICKEDpro <janroyal45@gmail.com>
2023-01-28 05:32:14 +0100

    convert file Signature

--- lib/business_logic/cloud_convert/cloudconver_api_methods.dart
+++ lib/business_logic/cloud_convert/cloudconver_api_methods.dart
@@ -1,12 +1,12 @@
-import 'dart:io';
-
 import "package:http/http.dart" as http;
 
 import '../../bloc/file_selection_bloc/file_state.dart';
 
 class CloudConvertMethods {
   //import file
-  Future<Map<String, dynamic>> convert(FileState file) async {
+  Future<Map<String, dynamic>> convertFile(FileState file) async {
+    http.post(Uri.parse(""),
+        body: <String, dynamic>{}, headers: {"Authorization": "Bearer "});
     return {};
   }
 }
