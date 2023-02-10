bf17e99f9faf0cde89751aa186d5be8d6839489c
janWICKEDpro <janroyal45@gmail.com>
2023-02-10 08:04:31 +0100

    Created a directory for downloaded files

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -21,7 +21,7 @@ class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
     return list.isEmpty
-        ? const Text("No FIles yet")
+        ? const Center(child: Text("No FIles yet"))
         : ListView.builder(
             itemCount: list.length,
             itemBuilder: (context, index) {
