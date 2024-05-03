ee455f7703d61293ffe92e59a8ca979bc883b53c
janWICKEDpro <janroyal45@gmail.com>
2024-05-03 22:12:08 +0100

    non nullable file path

--- lib/data_layer/open_files.dart
+++ lib/data_layer/open_files.dart
@@ -3,5 +3,5 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 
 Future openFile(PlatformFile convertedFile) async {
-  final file = File(convertedFile.path);
+  final file = File(convertedFile.path!);
 }
