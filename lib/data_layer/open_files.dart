e16ec048d82c638c93863c5116c76bf39e6cbd16
janWICKEDpro <janroyal45@gmail.com>
2024-05-03 22:21:42 +0100

    added url launcher instead to open files using default file associaition

--- lib/data_layer/open_files.dart
+++ lib/data_layer/open_files.dart
@@ -1,7 +1,15 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Future openFile(PlatformFile convertedFile) async {
   final file = File(convertedFile.path!);
+  final Uri uri = Uri.file(file.path);
+  if (!File(uri.toFilePath()).existsSync()) {
+    throw Exception('$uri does not exist!');
+  }
+  if (!await launchUrl(uri)) {
+    throw Exception('Could not launch $uri');
+  }
 }
