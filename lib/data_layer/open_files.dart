e0b3a657d3e51486bd18ce89783c44271a5c599a
janWICKEDpro <janroyal45@gmail.com>
2024-05-03 22:11:30 +0100

    open file plugin

--- lib/data_layer/open_files.dart
+++ lib/data_layer/open_files.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 
-Future openFile(PlatformFile file) async {}
+Future openFile(PlatformFile convertedFile) async {
+  final file = File(convertedFile.path);
+}
