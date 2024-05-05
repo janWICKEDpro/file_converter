6b9663cbad6cd483493f6a052012cd2f8c079d14
janWICKEDpro <janroyal45@gmail.com>
2024-05-05 20:08:04 +0100

    changed dart sdk version

---
+++ lib/business_logic/bloc/history_event.dart
@@ -0,0 +1,4 @@
+part of 'history_bloc.dart';
+
+@immutable
+sealed class HistoryEvent {}
---
+++ lib/business_logic/bloc/history_state.dart
@@ -0,0 +1,6 @@
+part of 'history_bloc.dart';
+
+@immutable
+sealed class HistoryState {}
+
+final class HistoryInitial extends HistoryState {}
