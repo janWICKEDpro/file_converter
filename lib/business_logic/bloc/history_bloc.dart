f3e5d2d4a33766ca768bbd77da692e7995284b98
janWICKEDpro <janroyal45@gmail.com>
2024-05-05 20:24:55 +0100

    added some storage permission logic

--- lib/business_logic/bloc/history_bloc.dart
+++ lib/business_logic/bloc/history_bloc.dart
@@ -1,12 +1,42 @@
 import 'dart:io';
-
+import 'package:file_converter/data_layer/list_files.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 part 'history_event.dart';
 part 'history_state.dart';
 
 class HistoryBloc extends Bloc<HistoryEvent, HistoryState> {
   HistoryBloc() : super(HistoryState()) {
-    on<Init>((event, emit) async {});
+    permissionInitializer();
+    on<Init>((event, emit) async {
+      emit(state.copyWith(loading: true));
+      if (state.permitted == true) {
+        final files = await listStuff();
+        emit(state.copyWith(convertedFiles: files));
+      } else {}
+    });
+  }
+
+  void permissionInitializer() async {
+    if (await checkPermission()) {
+      // ignore: invalid_use_of_visible_for_testing_member
+      emit(state.copyWith(permitted: true));
+    } else {
+      await requestPermission();
+      // ignore: invalid_use_of_visible_for_testing_member
+      emit(state.copyWith(permitted: await checkPermission()));
+    }
+  }
+
+  Future requestPermission() async => await Permission.storage.request();
+
+  Future<bool> checkPermission() async {
+    var status = await Permission.storage.status;
+    return status.isGranted;
+  }
+
+  Future<List<FileSystemEntity>> listStuff() async {
+    return listFiles();
   }
 }
--- lib/business_logic/bloc/history_state.dart
+++ lib/business_logic/bloc/history_state.dart
@@ -1,11 +1,11 @@
 part of 'history_bloc.dart';
 
 class HistoryState {
-  final List<File>? convertedFiles;
+  final List<FileSystemEntity>? convertedFiles;
   final bool? loading;
   HistoryState({this.convertedFiles, this.loading});
 
-  HistoryState copyWith({List<File>? convertedFiles, loading}) {
+  HistoryState copyWith({List<FileSystemEntity>? convertedFiles, loading}) {
     return HistoryState(
         convertedFiles: convertedFiles ?? this.convertedFiles,
         loading: loading ?? this.loading);
